ca4ea19a407c31c56f3eaaeff6f6043dd0242eb9
jemryl21 <prialdejemryl1@gmail.com>
2024-04-07 13:30:28 +0800

    Last commit

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -1,34 +1,34 @@
-const jwt = require('express-jwt');
-const { secret } = require('config.json');
+const { expressjwt: jwt } = require('express-jwt');
+const { secret } = require('../config.json');
 const db = require('_helpers/db');
 
 module.exports = authorize;
 
 function authorize(roles = []) {
-    //
-    //
-    if (typeof roles === 'string'){
+    // roles param can be a single role string (e.g. Role.User or 'User')
+    // or an array of roles (e.g. [Role.Admin, Role.User] or ['Admin', 'User'])
+    if (typeof roles === 'string') {
         roles = [roles];
     }
-    
+
     return [
-    //
-    jwt({ secret, algorithms: ['HS256']}),
 
-    //
-    async (req, res, next) => {
-        const account = await db.Account.findByPk(req.user.id);
+        jwt({ secret, algorithms: ['HS256']}),
 
-        if (!account || (roles.length && !roles.includes(account.role))) {
-            //
-            return res.status(401).json({ message: 'Unauthorized'});
+
+        async (req, res, next) => {
+            const account = await db.Account.findByPk(req.auth.id);
+
+            if (!account || (roles.length && !roles.includes(account.role))) {
+
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+
+
+            req.auth.role = account.role;
+            const refreshTokens = await account.getRefreshTokens();
+            req.auth.ownsToken = token => !!refreshTokens.find(x => x.token === token);
+            next();
         }
-
-        //
-        req.user.role = account.role;
-        const refreshToken = await account.getRefreshTokens();
-        req.user.ownsToken = token => !!refreshToken.find(x => x.token);
-        next();
-        }
-    ]
+    ];
 }
--- _middleware/validate-request.js
+++ _middleware/validate-request.js
@@ -1,13 +1,13 @@
 module.exports = validateRequest;
 
-function validateRequest(req, next , schema) {
+function validateRequest(req, next, schema) {
     const options = {
-        abortEarly: false, //
-        allowUnknown: true, //
-        stripUnknown: true, //
+        abortEarly: false, 
+        allowUnknown: true, 
+        stripUnknown: true 
     };
-    const { error, value} = schema.validate(req.body, options);
-    if(error) {
+    const { error, value } = schema.validate(req.body, options);
+    if (error) {
         next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
     } else {
         req.body = value;
